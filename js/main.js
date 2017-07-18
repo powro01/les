86b00d26d259ee2701a6cc6957f144a11b335915
powro01 <kamilpowroznik001@gmail.com>
2017-07-18 18:12:49 +0200

    working on rwd

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     setInterval(function() {
 
         var randomNumber = Math.floor((Math.random() * 9));
-        
+
         $(invests[randomNumber]).toggleClass('rotate-animation');
 
         setTimeout(function() {
@@ -12,45 +12,61 @@ $(document).ready(function() {
         }, 3000);
 
     }, 5000);
-    
+
     mySlider = new Siema();
 
     var sliderPrev = $('#sliderPrevArrow'),
         sliderNext = $('#sliderNextArrow');
 
-        sliderPrev.on('click', function() {
-            mySlider.prev();
-        });
+    sliderPrev.on('click', function() {
+        mySlider.prev();
+    });
 
-        sliderNext.on('click', function() {
-            mySlider.next();
-        });
+    sliderNext.on('click', function() {
+        mySlider.next();
+    });
 
     var modal = $('#modal'),
         modalCloseBtn = $('#modalCloseBtn'),
         modalImage = $('#modalImage');
 
-        modalCloseBtn.on('click', function() {
-            modal.removeClass('open');
-        });
+    modalCloseBtn.on('click', function() {
+        modal.removeClass('open');
+    });
 
     var modalImages = $('img.modal-image');
 
     modalImages.on('click', function(e) {
-        if($(this).data('modal') === true) {
+        if ($(this).data('modal') === true) {
             e.preventDefault();
             let source = $(this).attr('src');
             modalImage.attr('src', source);
             modal.addClass('open');
         }
     });
+
+    var flatTrigger = $('.flatTrigger'),
+        searchBox = $('#searchBox');
+
+    flatTrigger.on('click', function(e) {
+        e.preventDefault();
+        searchBox.toggleClass('open');
+    });
+
+    var menuTrigger = $('#menuTrigger'),
+        navigation = $('#navigation');
+
+    menuTrigger.on('click', function(e) {
+        e.preventDefault();
+        navigation.toggleClass('open');
+    })
 });
 
 function myMap() {
-        var myCenter = new google.maps.LatLng(49.972238,20.6138402);
-        var mapCanvas = document.getElementById("map");
-        var mapOptions = {center: myCenter, zoom: 16, scrollwheel: false, draggable: false};
-        var map = new google.maps.Map(mapCanvas, mapOptions);
-        var marker = new google.maps.Marker({position:myCenter});
-        marker.setMap(map);
-      }
+    var myCenter = new google.maps.LatLng(49.972238, 20.6138402);
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = { center: myCenter, zoom: 16, scrollwheel: false, draggable: false };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({ position: myCenter });
+    marker.setMap(map);
+}
